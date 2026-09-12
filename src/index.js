1b4470af55c4cda6416c5e3d9841fe34e3dcb9b2
Michael Pardun <96313540+mpardun@users.noreply.github.com>
2026-09-12 18:51:41 -0500

    testing added comment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // This is a simple function that takes a name as an argument and returns a greeting message.
+// This is another comment for testing.
 function sayHi(name) {
   return `Hello ${name}`
 }
